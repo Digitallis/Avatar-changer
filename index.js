2b45936a85f55c65c28e850fa4a3dbe81e0a3e39
bog <oskar.suc@gmail.com>
2021-03-08 12:23:08 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js.old');
+const client = new Discord.Client();
+const fs = require('fs');
+const CronJob = require('cron').CronJob;
+require('dotenv').config()
+
+function getRandomLine(filename) {
+  var data = fs.readFileSync(filename, "utf8");
+  var lines = data.split('\n');
+  return lines[Math.floor(Math.random() * lines.length)];
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  var job = new CronJob('0 20 */1 * *', function() {
+    var the_random_line_text = getRandomLine('./avatars.txt')
+    client.user.setAvatar(the_random_line_text);
+    console.log('pfp changed')
+  });
+  job.start();
+});
+
+client.login(process.env.BOT_TOKEN);
